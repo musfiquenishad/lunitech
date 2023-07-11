0748a70aa69c102ab6a0ba2641303f6677bd244c
musfiquenishad <ahsannishad@gmail.com>
2023-07-11 17:43:49 +0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.unsplash.com",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
-import heroImage from "../public/photo-1618004912476-29818d81ae2e.webp";
 import logo from "../public/Lunitech-website-logo.png";
 import logoLight from "../public/Lunitech-website-logo-light.png";
 import Image from "next/image";
 import Link from "next/link";
-const Home = () => {
+
+function about() {
   return (
     <div>
-      {/* Navigation */}
       <div className="bg-white pb-6 sm:pb-8 sm:px-32 lg:pb-12 ">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
           <header className="mb-8 flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-16">
@@ -14,6 +13,7 @@ const Home = () => {
             <Link href="/">
               <Image className="h-14 w-auto" src={logo} />
             </Link>
+
             {/* Navbar Start */}
             <nav className="hidden gap-12 lg:flex">
               <Link
@@ -69,44 +69,49 @@ const Home = () => {
               Menu
             </button>
           </header>
-
-          <section className="flex flex-col justify-between gap-6 sm:gap-10 md:gap-16 lg:flex-row">
-            <div className="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
-              <p className="mb-4 font-semibold text-gray-500 md:mb-6 md:text-lg xl:text-xl">
-                Very proud to introduce
-              </p>
-
-              <h1 className="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">
-                Lunitech inc
-              </h1>
-
-              <p className="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">
-                Lunitech is a globally recognized technology startup focused on
-                website development, web applications, software solutions, and
-                more.
-              </p>
-
-              <div className="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
-                <Link
-                  href="/contact"
-                  className="hidden rounded-lg bg-gray-700 px-8 py-2.5 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-500 focus-visible:ring active:text-gray-200 md:text-base lg:inline-block"
-                >
-                  About Us
-                </Link>
-              </div>
-            </div>
-
-            <div className="h-48 overflow-hidden rounded-lg bg-gray-50 shadow-lg lg:h-auto xl:w-5/12">
-              <Image
-                src={heroImage}
-                loading="lazy"
-                alt="Photo by Fakurian Design"
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-          </section>
         </div>
       </div>
+
+      {/* hero content start */}
+      <div>
+        <div class="bg-white py-6 sm:py-15 lg:py-12">
+          <div class="mx-auto max-w-screen-md px-4 md:px-8">
+            <h1 class="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6">
+              ABOUT US
+            </h1>
+
+            <p class="mb-6 text-gray-500 sm:text-lg md:mb-8">
+              LuniTech is a leading technology startup offering exceptional
+              website, web application, and software solutions. We empower
+              businesses globally to embrace digital transformation through
+              innovative and tailored services.
+            </p>
+
+            <blockquote class="mb-6 border-l-4 pl-4 italic text-gray-500 sm:text-lg md:mb-8 md:pl-6">
+              “From desktop applications to mobile apps and enterprise-level
+              software, we bring your ideas to life.”
+            </blockquote>
+
+            <p class="text-gray-500 sm:text-lg">
+              With a focus on quality, we follow rigorous testing methodologies
+              to ensure our deliverables exceed expectations. As your partner in
+              digital innovation, we provide customized solutions aligned with
+              your business objectives.
+            </p>
+            <p class="text-gray-500 sm:text-lg">
+              Contact us to embark on a transformative journey with LuniTech and
+              achieve remarkable success in the digital landscape.
+            </p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+
+      {/* hero content end */}
 
       {/* Footer starts */}
       <div className="bg-white dark:bg-gray-900">
@@ -123,8 +128,7 @@ const Home = () => {
                 </Link>
 
                 <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">
-                  Join 31,000+ other and never miss out on new tips, tutorials,
-                  and more.
+                  Lunitech is a globally recognized technology startup
                 </p>
 
                 <div className="flex mt-6 -mx-2">
@@ -277,6 +281,6 @@ const Home = () => {
       {/* Footer ends */}
     </div>
   );
-};
+}
 
-export default Home;
+export default about;
