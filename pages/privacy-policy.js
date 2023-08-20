5b162351ed4d8aa12f1d39c9e98ac096f6355e5b
musfiquenishad <ahsannishad@gmail.com>
2023-08-21 00:43:18 +0600

    Privacy policy fixed

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -160,7 +160,7 @@ function privacyPolicy() {
           <h3>Usage Data</h3>
           <p>Usage Data is collected automatically when using the Service.</p>
           <p>
-            Usage Data may include information such as Your Device's Internet
+            Usage Data may include information such as Your Devices Internet
             Protocol address (e.g. IP address), browser type, browser version,
             the pages of our Service that You visit, the time and date of Your
             visit, the time spent on those pages, unique device identifiers and
@@ -208,7 +208,7 @@ function privacyPolicy() {
               <p>
                 <strong>To contact You:</strong> To contact You by email,
                 telephone calls, SMS, or other equivalent forms of electronic
-                communication, such as a mobile application's push notifications
+                communication, such as a mobile applications push notifications
                 regarding updates or informative communications related to the
                 functionalities, products or contracted services, including the
                 security updates, when necessary or reasonable for their
@@ -310,8 +310,8 @@ function privacyPolicy() {
           <h2>Transfer of Your Personal Data</h2>
           <p>
             Your information, including Personal Data, is processed at the
-            Company's operating offices and in any other places where the
-            parties involved in the processing are located. It means that this
+            Companys operating offices and in any other places where the parties
+            involved in the processing are located. It means that this
             information may be transferred to — and maintained on — computers
             located outside of Your state, province, country or other
             governmental jurisdiction where the data protection laws may differ
@@ -389,7 +389,7 @@ function privacyPolicy() {
             commercially acceptable means to protect Your Personal Data, We
             cannot guarantee its absolute security.
           </p>
-          <h1>Children's Privacy</h1>
+          <h1>Childrens Privacy</h1>
           <p>
             Our Service does not address anyone under the age of 13. We do not
             knowingly collect personally identifiable information from anyone
@@ -402,15 +402,15 @@ function privacyPolicy() {
           <p>
             If We need to rely on consent as a legal basis for processing Your
             information and Your country requires consent from a parent, We may
-            require Your parent's consent before We collect and use that
+            require Your parents consent before We collect and use that
             information.
           </p>
           <h1>Links to Other Websites</h1>
           <p>
             Our Service may contain links to other websites that are not
             operated by Us. If You click on a third party link, You will be
-            directed to that third party's site. We strongly advise You to
-            review the Privacy Policy of every site You visit.
+            directed to that third partys site. We strongly advise You to review
+            the Privacy Policy of every site You visit.
           </p>
           <p>
             We have no control over and assume no responsibility for the
